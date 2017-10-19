ad6f96555e47be88750dc674795f4f1023e83197
Ryo Ota <nwtgck@gmail.com>
2017-10-19 11:28:11 +0900

    [add] Create one-group chat

--- README.md
+++ README.md
@@ -2,6 +2,14 @@
 
 ## How to connect WebSocket
 
+### simple echo WS
+
 ```bash
 $ wscat -c localhost:9000/echo-w
+```
+
+### one-group chat
+
+```bash
+$ wscat -c localhost:9000/dynamic-ws
 ```
--- app/controllers/WebSocketController.scala
+++ app/controllers/WebSocketController.scala
@@ -2,7 +2,9 @@ package controllers
 
 import javax.inject._
 
-import akka.stream.scaladsl.Flow
+import akka.actor.ActorSystem
+import akka.stream.{ActorMaterializer, KillSwitches, UniqueKillSwitch}
+import akka.stream.scaladsl.{BroadcastHub, Flow, Keep, MergeHub, Sink}
 import play.api.mvc._
 
 /**
@@ -12,7 +14,35 @@ import play.api.mvc._
 @Singleton
 class WebSocketController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
-  def connect(): WebSocket = WebSocket.accept{req =>
+  def connect(): WebSocket = WebSocket.accept{ req =>
     Flow.fromFunction[String, String](txt => s"flow passed! '$txt'")
+  }
+
+  def dynamic(): WebSocket = WebSocket.accept[String, String]{ req=>
+    DynamicStream.publishSubscribeFlow
+  }
+}
+
+// (from: https://speakerdeck.com/wadayusuke/implement-stream-server-with-akka-streams-and-websocket)
+object DynamicStream{
+  implicit val as  = ActorSystem()
+  implicit val mat = ActorMaterializer()
+
+  import scala.concurrent.duration._
+
+  lazy val publishSubscribeFlow: Flow[String, String, UniqueKillSwitch] = {
+    val (sink, source) =
+      MergeHub.source[String](perProducerBufferSize = 16)
+        .toMat(BroadcastHub.sink(bufferSize = 256))(Keep.both)
+        .run()
+
+    source.runWith(Sink.ignore)
+
+    val busFlow: Flow[String, String, UniqueKillSwitch] =
+      Flow.fromSinkAndSource(sink, source)
+          .joinMat(KillSwitches.singleBidi[String, String])(Keep.right)
+          .backpressureTimeout(3.seconds)
+
+    busFlow
   }
 }
